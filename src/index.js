6f561768d3844167136ea3dc58f957c148b63520
zemse <sohamzemse143@gmail.com>
2019-09-03 01:14:24 +0530

    add more buttons

--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ const model = {
   count: 0
 };
 
-const Button = {
-  view: vnode => m('button', {
-    onclick: () => {
-      model.count++
-    }
-  }, 'Increase counter')
-}
-
 m.mount(document.body, {
   view: () => [
     m('p', model.count),
-    m(Button, {name: 'Soham'})
+    m('button', {onclick: () => model.count++} ,'Increase counter'),
+    m('button', {onclick: () => model.count--} ,'Decrease counter'),
+    m('button', {onclick: () => model.count*=5} ,'Multiply by 5')
   ]
 });
