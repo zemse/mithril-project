6ea944ef20996dc056b1e0bed7c88fbdeef23f87
zemse <sohamzemse143@gmail.com>
2019-09-03 01:04:57 +0530

    create a counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const m = require('mithril');
 
-m.render(document.body, 'Hiii');
+const model = {
+  count: 0
+};
+
+const Button = {
+  view: vnode => m('button', {
+    onclick: () => {
+      model.count++
+    }
+  }, 'Increase counter')
+}
+
+m.mount(document.body, {
+  view: () => [
+    m('p', model.count),
+    m(Button, {name: 'Soham'})
+  ]
+});
